7c7873373603ae7a7fcc483bba07c40149252a4f
lukas <np-hardass@protonmail.com>
2020-09-25 10:47:41 +0200

    make the default state 'normal and disactivate the evil-collection

--- inits/evil.el
+++ inits/evil.el
@@ -1,8 +1,9 @@
 (use-package evil
-  :init
-  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-  (setq evil-want-keybinding nil)
-  (setq evil-shift-width 2)
+  :custom
+  (evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (evil-want-keybinding nil)
+  (evil-shift-width 2)
+  (evil-default-state 'normal)
   :config
   (evil-mode 1))
 
@@ -16,15 +17,15 @@
   :general (general-define-key
 			:states 'normal
 			"c c" 'evilnc-comment-or-uncomment-lines))
-			
+
 (use-package evil-surround
   :ensure t
   :init
   (global-evil-surround-mode))
 
-(use-package evil-collection
-  :after evil
-  :custom
-  (evil-collection-setup-minibuffer t)
-  (evil-collection-company-use-tng nil)
-  :init (evil-collection-init))
+;; (use-package evil-collection
+;;   :after evil
+;;   :custom
+;;   (evil-collection-setup-minibuffer t)
+;;   (evil-collection-company-use-tng nil)
+;;   :init (evil-collection-init))
