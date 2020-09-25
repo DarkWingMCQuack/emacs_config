3df711715df0754f65f015438f16a494f05dde71
lukas <np-hardass@protonmail.com>
2020-09-25 10:40:30 +0200

    use :custom

--- inits/dashboard.el
+++ inits/dashboard.el
@@ -1,14 +1,15 @@
 (use-package dashboard
-  :init
-  (setq dashboard-startup-banner "~/.emacs.d/gnu2.png")
-  (setq dashboard-image-banner-max-height 10)
-  (setq dashboard-init-info nil)
-  (setq dashboard-set-init-info nil)
-  (setq dashboard-set-footer nil)
-  (setq dashboard-items '((recents  . 5)
-                          (projects . 5)
-                          (agenda . 5)
-                          (registers . 5)))
+  :custom
+  (dashboard-startup-banner "~/.emacs.d/gnu2.png")
+  (dashboard-image-banner-max-height 10)
+  (dashboard-init-info nil)
+  (dashboard-set-init-info nil)
+  (dashboard-set-footer nil)
+  (dashboard-items '((recents  . 5)
+					 (projects . 5)
+					 (agenda . 5)
+					 (registers . 5)))
+  (initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
+
   :config
-  (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
   (dashboard-setup-startup-hook))
