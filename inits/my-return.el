1713917639ce1eb82a274d9a2b895153aa423e8d
lukas <np-hardass@protonmail.com>
2020-09-26 20:53:38 +0200

    refactored the super-return

--- inits/my-return.el
+++ inits/my-return.el
@@ -4,46 +4,6 @@
 (require 'smartparens (concat user-emacs-directory
 							  (convert-standard-filename "inits/smartparens.el")))
 
-(defun advanced-return ()
-  "Advanced `newline' command for comment.  This function redefine <Enter> to
-provide a corrent comment symbol at each newline plus a space when you press
-<Enter> in the comment.  It also support JavaDoc style comment -- insert a `*'
-at the beggining of the new line if inside of a comment."
-  (interactive "*")
-  (let* ((last (point))
-         (line-beginning 
-		  (progn (beginning-of-line) 
-				 (point)))
-
-		 (is-inside-oneline-comment
-		  (let* ((line (thing-at-point 'line t))
-				 (regexp (concat "[[:space:]]*" comment-start ".*$")))
-			(string-match regexp line))
-		  )
-
-         (is-inside-multiline-comment
-          (progn
-            (goto-char last)
-            (if (search-backward "*/" nil t)
-                ;; there are some comment endings - search forward
-                (search-forward "/*" last t)
-              ;; it's the only comment - search backward
-              (goto-char last)
-              (search-backward "/*" nil t)))))
-
-    ;; go to last char position
-    (goto-char last)
-
-    (if is-inside-oneline-comment
-		(comment-indent-new-line)
-      ;; else we check if it is java-doc style comment.
-      (if is-inside-multiline-comment
-          (progn
-			(insert "\n* ")
-			(indent-for-tab-command))
-        ;; else insert only new-line
-        (newline-and-indent)))))
-
 (defun delete-ws-forward ()
   "Deletes all whitespace in front of cursor."
   (interactive)
@@ -64,25 +24,91 @@ at the beggining of the new line if inside of a comment."
                      (backward-char))
                    (point))))
 
-(defun my-super-return ()
-  "My super return check for c like languages.
-if we are in an sexp and dont have a , before the cursor
-we delete ws before and after the cursor and then jump out of the sexp
-if not we just trigger my-doxygen-return"
+(defun is-inside-oneline-comment ()
+  "check if the cursor is currently in a oneline comment"
   (interactive)
-  (progn
-    (if (and
-         (looking-at-p "[[:space:]]*)")
-         (not
-          (looking-back 
-		   ",[[:space:]]*" 
-		   nil)))
-        (progn
-          (delete-ws-forward)
-          (delete-ws-backward)
-          (sp-up-sexp))
-      (advanced-return))))
 
-;;activate my super return in every prog enviroment
-(add-hook 'prog-mode-hook (lambda ()
-                            (local-set-key (kbd "<RET>") 'my-super-return)))
+  (let* ((line (thing-at-point 'line t))
+		 (regexp (concat "[[:space:]]*" comment-start "*.*$")))
+	(string-match regexp line)))
+
+;; TODO: create a more sophisticated method for finding this out
+;; since this fails if any string contains /*
+(defun is-inside-multiline-comment ()
+  "check if the cursor is currently in a multiline comment"
+  (interactive)
+
+  (let* ((last (point))
+         (line-beginning 
+		  (progn (beginning-of-line) 
+				 (point))))
+
+    (progn
+	  ;; maybe we need this
+      ;; (goto-char last)
+	  (let ((result 
+			 (if (search-backward "*/" nil t)
+				 ;; there are some comment endings - search forward
+				 (search-forward "/*" last t)
+			   ;; it's the only comment - search backward
+			   (goto-char last)
+			   (search-backward "/*" nil t)))
+			) (progn
+			(goto-char last)
+			result
+			))
+      )))
+
+(defun my-super-return ()
+  "My super return check for programming languages.
+if we are in an sexp and dont have a , before the cursor and are not in a lisp
+we delete ws before and after the cursor and then jump out of the sexp.
+If the next character is a ; we just jump behind it.
+If the cursor currently is in a singleline comment the next line is a comment as well
+If the cursor is in a block comment the next line starts with a *
+"
+  (interactive)
+  (cond ((and
+		  (looking-at-p "[[:space:]]*)")
+		  (not
+		   (looking-back 
+			",[[:space:]]*" 
+			nil))
+		  ;; do not jump out of sexp if the major mode is a lisp-mode
+		  (not 
+		   (or 
+			(derived-mode-p 'lisp-mode)
+			(derived-mode-p 'emacs-lisp-mode))))
+		 (progn
+		   (delete-ws-forward)
+		   (delete-ws-backward)
+		   (sp-up-sexp)))
+
+		;; jump one char if the next char is just a ;
+		((and
+		  (looking-back ".*[[:space:]]*" nil)
+		  (looking-at-p "[[:space:]]*;")
+		  (not (string-match  ";+" comment-start)))
+		 (progn
+		   (delete-ws-forward)
+		   (delete-ws-backward)
+		   (forward-char)))
+
+		;; create newline with comment if current line is oneline comment
+		((is-inside-oneline-comment)
+		 (comment-indent-new-line))
+
+		;; create newline with * if current line is block comment
+		((is-inside-multiline-comment)
+         (progn
+		   (insert "\n* ")
+		   (indent-for-tab-command))
+		 )
+
+		(t (newline-and-indent))))
+
+
+(general-define-key
+ :keymaps 'prog-mode-map
+ :states 'insert
+ "RET" 'my-super-return)
