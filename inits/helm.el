a04f1650189066176feee081582ead391957e72a
lukas <np-hardass@protonmail.com>
2020-09-29 13:07:02 +0200

    use :custom

--- inits/helm.el
+++ inits/helm.el
@@ -5,13 +5,14 @@
   ("M-x" . helm-M-x)
   ("M-p" . helm-show-kill-ring)
 
+  :custom
+  (helm-M-x-fuzzy-match t)
+  (helm-buffers-fuzzy-matching t)
+  (helm-display-header-line nil)
+
   :config
-  (setq helm-M-x-fuzzy-match t
-        helm-buffers-fuzzy-matching t
-        helm-display-header-line nil)
   (helm-mode 1)
   (helm-autoresize-mode 1))
 
 (use-package swiper-helm
-  :ensure t
   :bind ("C-s" . swiper-helm))
