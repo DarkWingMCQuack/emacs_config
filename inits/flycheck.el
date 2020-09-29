debed3bc1ba842dd80a575eca10670c240d2651e
lukas <np-hardass@protonmail.com>
2020-09-29 15:09:44 +0200

    added flyspell and flycheck-popup

--- inits/flycheck.el
+++ inits/flycheck.el
@@ -1,3 +1,17 @@
 (use-package flycheck
   :init
   (global-flycheck-mode))
+
+(use-package flyspell
+  :hook
+  (prog-mode-hook . flyspell-prog-mode)
+  (text-mode-hook . flyspell-mode)
+
+  :general
+  (general-define-key
+   :keymaps 'flyspell-mode-map
+   :states 'normal
+   "c w" 'flyspell-correct-word))
+
+(use-package flycheck-popup-tip
+  :hook (global-flycheck-mode . flycheck-popup-tip-mode))
