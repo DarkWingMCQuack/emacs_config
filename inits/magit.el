6729e065386ac6c0623f378c9f967b4d326d6fbd
lukas <np-hardass@protonmail.com>
2020-09-29 15:23:20 +0200

    map q to buffer deletion in magit

--- inits/magit.el
+++ inits/magit.el
@@ -1,5 +1,13 @@
 (use-package magit
-  :commands magit-status)
+  :commands magit-status
+  :general
+
+  (general-define-key
+   :keymaps 'magit-mode-map
+   :states 'normal
+   "q" (lambda()
+		 (interactive)
+		 (magit-mode-bury-buffer t))))
 
 (use-package evil-magit
   :after evil
@@ -7,9 +15,9 @@
   (evil-magit-init)
   :general
   (my-leader
-   "g g" '(magit-status :wk "magit status")
-   "g t" '(magit-todos-list :wk "show todos")
-   "g p" '(magit-push :wk "push")))
+	"g g" '(magit-status :wk "magit status")
+	"g t" '(magit-todos-list :wk "show todos")
+	"g p" '(magit-push :wk "push")))
 
 (use-package magit-todos
   :after magit
