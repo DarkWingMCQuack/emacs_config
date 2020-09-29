b06e289967b6292307d563c56a79816360f8f8c6
lukas <np-hardass@protonmail.com>
2020-09-29 15:37:44 +0200

    some refactoring

--- inits/go.el
+++ inits/go.el
@@ -1,5 +1,5 @@
 (use-package go-mode
   :mode "\\.go\\'"
-  :custom (gofmt-command "goimports")
-  :config
-  (use-package gotest))
+  :custom (gofmt-command "goimports"))
+
+(use-package gotest)
--- inits/scala.el
+++ inits/scala.el
@@ -6,13 +6,13 @@
 (use-package sbt-mode
   :commands sbt-start sbt-command
   :pin melpa
+  :custom
+  ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+  (sbt:program-options '("-Dsbt.supershell=false"))
   :config
   ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
   ;; allows using SPACE when in the minibuffer
   (substitute-key-definition
    'minibuffer-complete-word
    'self-insert-command
-   minibuffer-local-completion-map)
-   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
-   (setq sbt:program-options '("-Dsbt.supershell=false"))
-)
+   minibuffer-local-completion-map))
