b2c966a4f37ad69f7db02f052f466b5bd04aa89c
lukas <np-hardass@protonmail.com>
2020-10-11 20:04:06 +0200

    init undo-tree mode everywhere

--- inits/undotree.el
+++ inits/undotree.el
@@ -1,9 +1,16 @@
 (use-package undo-tree
   :diminish
+  :init
+  (global-undo-tree-mode 1)
+
   :config
   (custom-set-variables
    '(undo-tree-history-directory-alist
 	 (quote (("." . "~/.emacs.d/undo/")))))
+
+  :custom
+  (undo-tree-visualizer-timestamps t)
+  (undo-tree-auto-save-history t)
 
   :general
   (my-leader
