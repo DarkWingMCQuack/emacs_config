25498d55ff5e26ac9a19de42272e449e3cf938ff
lukas <np-hardass@protonmail.com>
2021-07-09 23:19:53 +0200

    dont do stupid stuff when loading company

--- inits/company.el
+++ inits/company.el
@@ -7,41 +7,10 @@
   (company-minimum-prefix-length 1)
   (company-echo-delay 0)
   (company-auto-commit nil)
-  (company-show-numbers t)
+  (company-show-numbers nil)
   (company-tooltip-limit 15)
   (company-selection-wrap-around t)
   (company-require-match 'never)
-
-  :config
-  (delete 'company-dabbrev company-backends)
-  (delete 'company-nxml company-backends)
-  (delete 'company-eclim company-backends)
-  (delete 'company-xcode company-backends)
-  (delete 'company-capf company-backends)
-  (delete 'company-dabbrev-code company-backends)
-  (delete 'company-gtags company-backends)
-  (delete 'company-etags company-backends)
-  (delete 'company-clang company-backends)
-  (delete 'company-bbdb company-backends)
-  (delete 'company-css company-backends)
-  (delete 'company-semantic company-backends)
-
-  (defun ora-company-number ()
-	"Forward to `company-complete-number'.
-     Unless the number is potentially part of the candidate.
-     In that case, insert the number."
-	(interactive)
-	(let* ((k (this-command-keys))
-           (re (concat "^" company-prefix k)))
-      (if (cl-find-if (lambda (s) (string-match re s))
-                      company-candidates)
-          (self-insert-command 1)
-		(company-complete-number (string-to-number k)))))
-
-  (mapc
-   (lambda (x)
-	 (define-key company-active-map (format "%d" x) 'ora-company-number))
-   (number-sequence 0 9))
 
   :general
   (general-define-key
