f620e76e902800812d45b6d1618704fb4d688628
lukas <np-hardass@protonmail.com>
2021-07-10 17:51:40 +0200

    automatically focus help windows

--- core/core-env.el
+++ core/core-env.el
@@ -6,6 +6,7 @@
 (setq gc-cons-threshold 300000000)
 
 (setq read-process-output-max (* 3 1024 1024)) ;; 3mb
+(setq help-window-select t)
 
 ;; ensure environment variables inside Emacs look the same as in the user's shell.
 (require 'exec-path-from-shell)
