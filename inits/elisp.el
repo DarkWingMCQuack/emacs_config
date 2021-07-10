61d8ae14c21effa1a9f90493ecdc39ce915d392a
lukas <np-hardass@protonmail.com>
2021-07-10 15:43:04 +0200

    format elisp files on save

--- inits/elisp.el
+++ inits/elisp.el
@@ -10,6 +10,12 @@
 
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'recompile-elc-on-save)
+
+;; format elisp files on save
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook 'my-format-buffer nil 'make-it-local)))
+
 (with-eval-after-load 'flycheck
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
