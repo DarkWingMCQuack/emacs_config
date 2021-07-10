097dd1ee4eb37f1d76af03fe77e583ad6ea0b1a3
lukas <np-hardass@protonmail.com>
2021-07-10 17:52:24 +0200

    better lsp ux

--- inits/lsp.el
+++ inits/lsp.el
@@ -1,24 +1,34 @@
 (use-package lsp-mode
   :custom
+  ;; performance
+  (lsp-idle-delay 0.500)
+  (lsp-log-io nil)
+  (lsp-print-io nil)
+  (lsp-trace nil)
+  (lsp-print-performance nil)
   (lsp-completion-provider :capf)
+
   (company-lsp-cache-candidates t) ;; auto, t(always using a cache), or nil
   (company-lsp-async t)
   (company-lsp-enable-snippet t)
   (company-lsp-enable-recompletion t)
+
+  (lsp-auto-guess-root t)
   (lsp-enable-snippet t)
-  (lsp-print-io nil)
   (lsp-enable-completion-at-point t)
   (lsp-enable-xref t)
   (lsp-enable-indentation t)
-  (lsp-semantic-tokens-enable nil)
   (lsp-signature-auto-activate t)
   (lsp-signature-render-documentation t)
   (lsp-enable-text-document-color t)
+
+  (lsp-semantic-tokens-enable nil)
   (lsp-headerline-breadcrumb-enable nil)
-  (lsp-trace nil)
-  (lsp-print-performance nil)
-  (lsp-auto-guess-root t)
   (lsp-modeline-code-actions-segments '(count icon name))
+
+  (lsp-signature-function 'lsp-signature-posframe)
+
+
   (lsp-clients-clangd-args
    '("-j=4"
 	 "--background-index"
@@ -34,17 +44,19 @@
   (rust-mode . lsp)
   (scala-mode . lsp)
   (lsp-mode . lsp-lens-mode)
+  (lsp-mode . lsp-signature-mode)
   ;; only format-on-save if lsp-mode is active
   (lsp-mode . (lambda ()
 				(add-hook 'before-save-hook 'lsp-format-buffer nil 'make-it-local)))
 
-  
 
   :general
   (my-leader 'lsp-mode-map
 	:states 'normal
 	"TAB" '(lsp-format-buffer :wk "format buffer")
-	"s d" '(lsp-ui-doc-glance :wk "show documentation"))
+	"s d" '(lsp-describe-thing-at-point :wk "show documentation")
+	"s r" '(lsp-find-references :wk "show documentation")
+	"d"   '(lsp-ui-doc-glance :wk "peek documentation"))
 
   (general-define-key
    :keymaps 'lsp-mode-map
@@ -57,27 +69,14 @@
   :after lsp-mode
   :custom
 
-  (lsp-ui-doc-enable t)
-  (lsp-ui-doc-header t)
-  (lsp-ui-doc-show-with-cursor nil)
-  (lsp-ui-doc-show-with-mouse nil)
-  (lsp-ui-doc-include-signature t)
+  (lsp-ui-doc-enable nil)
   (lsp-ui-doc-position 'at-point) ;; top, bottom, or at-point
-  (lsp-ui-doc-max-width 120)
-  (lsp-ui-doc-max-height 30)
-  ;; (lsp-ui-doc-use-childframe t)
-  (lsp-ui-doc-use-webkit t)
-  (lsp-ui-doc-focus-frame t)
 
   ;; lsp-ui-flycheck
   (lsp-ui-flycheck-enable t)
 
-  ;; lsp-ui-sideline
   (lsp-ui-sideline-enable nil)
-  (lsp-ui-peek-enable t)
-  (lsp-ui-peek-peek-height 20)
-  (lsp-ui-peek-list-width 50)
-  (lsp-ui-peek-fontify 'on-demand) ;; never, on-demand, or always
+  (lsp-ui-peek-enable nil)
 
   :hook (lsp-mode . lsp-ui-mode))
 
