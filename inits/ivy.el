6e3f5ae2fe9234f1b8ffd143d212a6ab6a5ff785
lukas <np-hardass@protonmail.com>
2021-07-10 15:23:23 +0200

    use ivy-xref and rebind M-x to counsel-M-x

--- inits/ivy.el
+++ inits/ivy.el
@@ -12,7 +12,9 @@
   (counsel-mode)
   :general
   (my-leader
-	"f f" '(counsel-find-file :wk "find file")))
+	"f f" '(counsel-find-file :wk "find file"))
+  (general-define-key
+   "M-x" 'counsel-M-x))
 
 (use-package counsel-projectile
   :config
@@ -29,7 +31,7 @@
 ;; More friendly interface for ivy
 ;; https://github.com/Yevgnen/ivy-rich
 (use-package ivy-rich
-  :after (ivy counsel-projectile counsel)
+  :after (ivy counsel-projectile counsel ivy-xref)
   :config
   (ivy-rich-mode 1)
   :custom
@@ -41,6 +43,16 @@
 ;; https://github.com/seagle0128/all-the-icons-ivy-rich/
 (use-package all-the-icons-ivy-rich
   :init
-  (all-the-icons-ivy-rich-mode 1)
+  (all-the-icons-ivy-rich-mode 1))
+
+(use-package ivy-xref
+  :init
+  ;; xref initialization is different in Emacs 27 - there are two different
+  ;; variables which can be set rather than just one
+  (when (>= emacs-major-version 27)
+    (setq xref-show-definitions-function #'ivy-xref-show-defs))
+  ;; Necessary in Emacs <27. In Emacs 27 it will affect all xref-based
+  ;; commands other than xref-find-definitions (e.g. project-find-regexp)
+  ;; as well
   :custom
-  (all-the-icons-ivy-rich-icon-size 0.8))
+  (xref-show-xrefs-function #'ivy-xref-show-xrefs "Use Ivy to show xrefs"))
